2b740907d1342927ac1075174c297e9917615823
AszAr1 <aszaslavagutkovski1605@gmail.com>
2023-08-24 19:58:26 +0300

    maintenance

--- backend/src/ShopPrj/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('products.urls')),
-    path('', include('users.urls')),
-    path('admin/', admin.site.urls),
-]
--- backend/src/products/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Order, OrderItem, Product, Favorite, CartItem
-
-
-admin.site.register([Order, OrderItem, Product, Favorite, CartItem ])
