781a4b006d13f99830314c78d440e8234814b6f6
AszAr1 <aszaslavagutkovski1605@gmail.com>
2023-08-25 03:53:20 +0300

    fix some issues

--- backend/ShopPrj/settings.py
+++ backend/ShopPrj/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'products',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ShopPrj.urls'
@@ -135,3 +137,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
