f56a14a492535cfac96304ad53e7ebbba4a5386d
AszAr1 <aszaslavagutkovski1605@gmail.com>
2023-11-18 11:34:01 +0300

    changed toke lifetime

--- backend/ShopPrj/settings.py
+++ backend/ShopPrj/settings.py
@@ -153,10 +153,10 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 
 
