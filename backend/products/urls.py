8f4939dc8ffd6f2f706fbed4dc6db1aede4cd4fd
AszAr1 <aszaslavagutkovski1605@gmail.com>
2023-12-09 22:20:27 +0300

    new products/urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import (MainAPIView, 
+from .views import (AddCartItemAPIView, 
+                    AddFavoriteAPIView, 
+                    MainAPIView, 
                     ProductDetailAPIView, 
                     FavoritesAPIView, 
                     CartAPIView, 
@@ -13,10 +15,12 @@ from .views import (MainAPIView,
 
 urlpatterns = [
     path('', MainAPIView.as_view(), name='main'),
-    path('products/hoodies/', HoodiesListAPIView.as_view(), name='product-hoodies'),
-    path('products/sneakers/', SneakersListAPIView.as_view(), name='product-sneakers'),
+    path('products/hoodies/', HoodiesListAPIView.as_view(), name='hoodies'),
+    path('products/sneakers/', SneakersListAPIView.as_view(), name='sneakers'),
     path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
     path('favorites/', FavoritesAPIView.as_view(), name='favorites'),
+    path('favorites/add/', AddFavoriteAPIView.as_view(), name='add-favorite'),
     path('cart/', CartAPIView.as_view(), name='cart'),
+    path('cart/add/', AddCartItemAPIView.as_view(), name='add-cart-item'),
     path('orders/', OrdersAPIView.as_view(), name='orders'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
