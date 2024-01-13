2db84512a8147fdeb76ee14fc8ea1cec1bd1e0ec
AszAr1 <aszaslavagutkovski1605@gmail.com>
2024-01-13 08:38:10 +0300

    Readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+This project is a cooperation work with my friend, where we both did our respective parts, 
+me as a backend developer and him as a cd JJfrontend developer.
--- backend/users/models.py
+++ backend/users/models.py
@@ -7,3 +7,10 @@ class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     profile_picture = models.ImageField(null=True, blank=True)
     email = models.EmailField(verbose_name=("email address"))
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self) -> str:
+        return f"{self.username}"
